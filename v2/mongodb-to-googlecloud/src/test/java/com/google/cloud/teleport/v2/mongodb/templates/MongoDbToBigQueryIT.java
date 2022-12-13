fb172cd0deb27e4f4569a9ec49128bbdc8e0e2a5
Jeffrey Kinard <jeff@thekinards.com>
2022-12-13 10:47:55 -0500

    Add timestamp field to BQ schema in MongoDBToBigQueryIT
    Signed-off-by: Jeffrey Kinard <jeff@thekinards.com>

--- v2/mongodb-to-googlecloud/src/test/java/com/google/cloud/teleport/v2/mongodb/templates/MongoDbToBigQueryIT.java
+++ v2/mongodb-to-googlecloud/src/test/java/com/google/cloud/teleport/v2/mongodb/templates/MongoDbToBigQueryIT.java
@@ -17,6 +17,7 @@ package com.google.cloud.teleport.v2.mongodb.templates;
 
 import static com.google.cloud.teleport.it.dataflow.DataflowUtils.createJobName;
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import com.google.cloud.bigquery.Field;
 import com.google.cloud.bigquery.Schema;
@@ -156,6 +157,7 @@ public final class MongoDbToBigQueryIT extends TemplateTestBase {
 
     String bqTable = testName.getMethodName();
     List<Field> bqSchemaFields = new ArrayList<>();
+    bqSchemaFields.add(Field.of("timestamp", StandardSQLTypeName.TIMESTAMP));
     mongoDocuments
         .get(0)
         .forEach((key, val) -> bqSchemaFields.add(Field.of(key, StandardSQLTypeName.STRING)));
@@ -189,7 +191,9 @@ public final class MongoDbToBigQueryIT extends TemplateTestBase {
     mongoDocuments.forEach(
         mongoDocument -> {
           JSONObject mongoDbJson = new JSONObject(mongoDocument.toJson());
-          mongoMap.put(mongoDbJson.getJSONObject(MONGO_DB_ID).getString("$oid"), mongoDbJson);
+          String mongoId = mongoDbJson.getJSONObject(MONGO_DB_ID).getString("$oid");
+          mongoDbJson.put(MONGO_DB_ID, mongoId);
+          mongoMap.put(mongoId, mongoDbJson);
         });
 
     TableResult tableRows = bigQueryClient.readTable(bqTable);
@@ -200,14 +204,11 @@ public final class MongoDbToBigQueryIT extends TemplateTestBase {
                 row.forEach(
                     val -> {
                       JSONObject bigQueryJson = new JSONObject(val.getStringValue());
-                      JSONObject bigQueryIdJson =
-                          new JSONObject(
-                              bigQueryJson.getString(MONGO_DB_ID).replaceFirst("=", ":"));
-                      String bigQueryOid = bigQueryIdJson.getString("$oid");
-                      bigQueryJson.put(MONGO_DB_ID, bigQueryIdJson);
+                      assertTrue(bigQueryJson.has("timestamp"));
 
-                      assertThat(mongoMap.get(bigQueryOid).toString())
-                          .isEqualTo(bigQueryJson.toString());
+                      bigQueryJson.remove("timestamp");
+                      String bigQueryId = bigQueryJson.getString(MONGO_DB_ID);
+                      assertTrue(mongoMap.get(bigQueryId).similar(bigQueryJson));
                     }));
   }
 
